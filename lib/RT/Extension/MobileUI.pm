b8d222b399ece20a830356e36855ea21ff03d112
Jesse Vincent <jesse@bestpractical.com>
2010-08-04 12:14:05 -0700

    Lots of cleanup

--- lib/RT/Extension/MobileUI.pm
+++ lib/RT/Extension/MobileUI.pm
@@ -5,4 +5,15 @@ package RT::Extension::MobileUI;
 
 my $VERSION = 1;
 
+sub MobileClient {
+    my $self = shift;
+
+
+if (($ENV{'HTTP_USER_AGENT'} || '') =~ /(?:hiptop|Blazer|Novarra|Vagabond|SonyEricsson|Symbian|NetFront|UP.Browser|UP.Link|Windows CE|MIDP|J2ME|DoCoMo|J-PHONE|PalmOS|PalmSource|iPhone|iPod|AvantGo|Nokia|Android)/io && !$HTML::Mason::Commands::session{'NotMobile'})  {
+    return 1;
+} else {
+    return undef;
+}
+
+}
 1;
