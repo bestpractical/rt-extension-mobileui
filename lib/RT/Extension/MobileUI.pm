269a8e55c5c8064132e6453f1121eda0023c7659
Jesse Vincent <jesse@bestpractical.com>
2010-08-05 12:17:18 -0700

    more releng

--- lib/RT/Extension/MobileUI.pm
+++ lib/RT/Extension/MobileUI.pm
@@ -5,6 +5,33 @@ package RT::Extension::MobileUI;
 
 our $VERSION = 0.9;
 
+
+=head1 NAME
+
+RT::Extension::MobileUI - A phone friendly web interface for RT
+
+=head1 DESCRIPTION
+
+This RT extension adds a mobile interface for RT.
+
+=head1 INSTALLATION
+
+    # perl Makefile.PL
+    # make
+    # make install
+
+    Add RT::Extension::MobileUI to your /opt/rt3/etc/RT_SiteConfig.pm file
+    Set(@Plugins, qw(RT::Extension::MobileUI));
+
+    If you have more than one Plugin enabled, you must enable them as one 
+    Set(@Plugins, qw(Foo Bar)); command
+
+    # restart apache
+=cut
+
+
+
+
 sub MobileClient {
     my $self = shift;
 
@@ -16,4 +43,15 @@ if (($ENV{'HTTP_USER_AGENT'} || '') =~ /(?:hiptop|Blazer|Novarra|Vagabond|SonyEr
 }
 
 }
+
+=head1 AUTHOR
+
+Jesse Vincent E<lt>jesse@bestpractical.comE<gt>
+
+=head1 LICENSE
+
+GPL version 2.
+
+=cut
+
 1;
